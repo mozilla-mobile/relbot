f593fe17465ea64c3bb4fd9bd24624ffd3ebc5f0
Stefan Arentz <stefan@arentz.ca>
2021-03-03 16:48:05 -0500

    Fixes #13 - Integrate A-S Releases automatically into A-C

--- src/android_components.py
+++ src/android_components.py
@@ -149,6 +149,159 @@ def _update_geckoview(ac_repo, fenix_repo, gv_channel, ac_major_version, author,
         raise e
 
 
+
+
+def validate_as_version(v):
+    """Validate that v is in the format of 63.0.2. Returns v or raises an exception."""
+    if not re.match(r"^\d+\.\d+\.\d+$", v):
+        raise Exception(f"Invalid A-S version format {v}")
+    return v
+
+def match_as_version(src):
+    """Find the A-S version in the contents of the given Dependencies.kt file."""
+    if match := re.compile(r'const val mozilla_appservices = "([^"]*)"', re.MULTILINE).search(src):
+        return validate_as_version(match[1])
+    raise Exception(f"Could not match mozilla_appservices in Dependencies.kt")
+
+def get_current_as_version(ac_repo, release_branch_name):
+    """Return the current as version used on the given release branch"""
+    content_file = ac_repo.get_contents("buildSrc/src/main/java/Dependencies.kt", ref=release_branch_name)
+    return match_as_version(content_file.decoded_content.decode('utf8'))
+
+def major_as_version_from_version(v):
+    """Return the major version for the given A-S version"""
+    c = validate_as_version(v).split(".")
+    return c[0]
+
+def as_version_sort_key(v):
+    a = v.split(".")
+    return int(a[0])*1000000 + int(a[1])*1000 + int(a[2])
+
+
+def get_latest_as_version(as_major_version):
+    """Find the last A-S version on Maven for the given major version"""
+
+    # Find the latest release in the multi-arch .aar
+
+    # TODO What is the right package to check here? full-megazord metadata seems broken.
+    r = requests.get(f"{MAVEN}/org/mozilla/appservices/nimbus/maven-metadata.xml?t={int(time.time())}")
+    r.raise_for_status()
+    metadata = xmltodict.parse(r.text)
+
+    versions = []
+    for version in metadata['metadata']['versioning']['versions']['version']:
+        if version.startswith(f"{as_major_version}."):
+            versions.append(version)
+
+    if len(versions) == 0:
+        raise Exception(f"Could not find any A-S {as_major_version} releases")
+
+    versions = sorted(versions, key=as_version_sort_key)
+    latest = versions[-1]
+
+    # Make sure this release has been uploaded for all architectures.
+
+    # TODO Do we need to do this?
+
+    #for arch in ("arm64-v8a", "armeabi-v7a", "x86", "x86_64"):
+    #    r = requests.get(f"{MAVEN}/org/mozilla/geckoview/{name}-{arch}/{latest}/{name}-{arch}-{latest}.pom?t={int(time.time())}")
+    #    r.raise_for_status()
+
+    return latest
+
+
+def compare_as_versions(a, b):
+    a = validate_as_version(a).split(".")
+    a = int(a[0])*1000000 + int(a[1])*1000 + int(a[2])
+    b = validate_as_version(b).split(".")
+    b = int(b[0])*1000000 + int(b[1])*1000 + int(b[2])
+    return a-b
+
+def _update_as_version(ac_repo, old_as_version, new_as_version, branch, author):
+    contents = ac_repo.get_contents("buildSrc/src/main/java/Dependencies.kt", ref=branch)
+    content = contents.decoded_content.decode("utf-8")
+    new_content = content.replace(f'mozilla_appservices = "{old_as_version}"',
+                                  f'mozilla_appservices = "{new_as_version}"')
+    if content == new_content:
+        raise Exception("Update to Dependencies.kt resulted in no changes: maybe the file was already up to date?")
+
+    ac_repo.update_file(contents.path, f"Update A-S to {new_as_version}.",
+                     new_content, contents.sha, branch=branch, author=author)
+
+
+
+
+def _update_application_services(ac_repo, fenix_repo, ac_major_version, author, debug, dry_run=False):
+    try:
+        release_branch_name = "master" if ac_major_version is None else f"releases/{ac_major_version}.0"
+        print(f"{ts()} Updating A-S on {ac_repo.full_name}:{release_branch_name}")
+
+        current_as_version = get_current_as_version(ac_repo, release_branch_name)
+        print(f"{ts()} Current A-S version on A-C {release_branch_name} is {current_as_version}")
+
+        latest_as_version = get_latest_as_version(major_as_version_from_version(current_as_version))
+        print(f"{ts()} Latest A-S version available is {latest_as_version}")
+
+        if compare_as_versions(current_as_version, latest_as_version) >= 0:
+            print(f"{ts()} No newer A-S release found. Exiting.")
+            return
+
+        print(f"{ts()} We should update A-C {release_branch_name} with A-S {latest_as_version}")
+
+        if dry_run:
+            print(f"{ts()} Dry-run so not continuing.")
+            return
+
+        #
+        # Check if the branch already exists
+        #
+
+        short_version = "master" if ac_major_version is None else f"{ac_major_version}"
+
+        # Create a non unique PR branch name for work on this ac release branch.
+        pr_branch_name = f"relbot/update-as/ac-{short_version}"
+
+        try:
+            pr_branch = ac_repo.get_branch(pr_branch_name)
+            if pr_branch:
+                print(f"{ts()} The PR branch {pr_branch_name} already exists. Exiting.")
+                return
+        except GithubException as e:
+            # TODO Only ignore a 404 here, fail on others
+            pass
+
+        #
+        # Create a new branch for this update
+        #
+
+        release_branch = ac_repo.get_branch(release_branch_name)
+        print(f"{ts()} Last commit on {release_branch_name} is {release_branch.commit.sha}")
+
+        ac_repo.create_git_ref(ref=f"refs/heads/{pr_branch_name}", sha=release_branch.commit.sha)
+        print(f"{ts()} Created branch {pr_branch_name} on {release_branch.commit.sha}")
+
+        #
+        # Update buildSrc/src/main/java/Gecko.kt
+        #
+
+        print(f"{ts()} Updating buildSrc/src/main/java/Dependencies.kt")
+        _update_as_version(ac_repo, current_as_version, latest_as_version, pr_branch_name, author)
+
+        #
+        # Create the pull request
+        #
+
+        print(f"{ts()} Creating pull request")
+        pr = ac_repo.create_pull(title=f"Update to A-S {latest_as_version} on {release_branch_name}",
+                                 body=f"This (automated) patch updates A-S to {latest_as_version}.",
+                                 head=pr_branch_name, base=release_branch_name)
+        print(f"{ts()} Pull request at {pr.html_url}")
+    except Exception as e:
+        # TODO Clean up the mess
+        raise e
+
+
+
 #
 # High Level Tasks
 #
@@ -160,6 +313,12 @@ def _update_geckoview(ac_repo, fenix_repo, gv_channel, ac_major_version, author,
 #
 
 def update_master(ac_repo, fenix_repo, author, debug, dry_run):
+    try:
+        _update_application_services(ac_repo, fenix_repo, None, author, debug, dry_run)
+    except Exception as e:
+        print(f"{ts()} Exception while updating A-S on A-C {ac_repo.name}:master: {str(e)}")
+    print()
+
     for gv_channel in ('nightly', 'beta', 'release'):
         try:
             _update_geckoview(ac_repo, fenix_repo, gv_channel, None, author, debug, dry_run)
@@ -173,6 +332,12 @@ def update_master(ac_repo, fenix_repo, author, debug, dry_run):
 
 def update_releases(ac_repo, fenix_repo, author, debug, dry_run):
     for ac_version in get_relevant_ac_versions(fenix_repo, ac_repo):
+        try:
+            _update_application_services(ac_repo, fenix_repo, ac_version, author, debug, dry_run)
+        except Exception as e:
+            print(f"{ts()} Exception while updating A-S on A-C {ac_version}: {str(e)}")
+        print()
+
         for gv_channel in ("beta", "release"):
             try:
                 _update_geckoview(ac_repo, fenix_repo, gv_channel, ac_version, author, debug, dry_run)
