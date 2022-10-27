490f21e9a48484d58550be853251d18e15f5f680
Gabriel Luong <gabriel.luong@gmail.com>
2022-10-27 17:38:59 -0400

    Bug 1797245 - Migrate Focus updates to work with the firefox-android monorepo

--- src/relbot.py
+++ src/relbot.py
@@ -117,7 +117,7 @@ if __name__ == "__main__":
     ac_repo = github.get_repo(f"{organization}/{repo_name_prefix}firefox-android")
     rb_repo = github.get_repo(f"{organization}/{repo_name_prefix}reference-browser")
     fenix_repo = github.get_repo(f"{organization}/{repo_name_prefix}fenix")
-    focus_repo = github.get_repo(f"{organization}/{repo_name_prefix}focus-android")
+    focus_repo = github.get_repo(f"{organization}/{repo_name_prefix}firefox-android")
 
     author_name = os.getenv("AUTHOR_NAME") or DEFAULT_AUTHOR_NAME
     author_email = os.getenv("AUTHOR_EMAIL") or DEFAULT_AUTHOR_EMAIL
